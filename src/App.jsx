5a6c79435592d79c3fb4d639170e0f12d4f27ce5
TiimmyTurnerDev <timmyturnerdev@gmail.com>
2022-09-28 21:12:53 +0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,30 @@ import appS from "./App.module.css";
 import Error from "./components/Error/Error";
 import Chat from "./components/Messenger/Chat/Chat";
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 function App() {
+  const apiKey = "928346fa14b100bf560110b1c5fef477";
+  const city = "Antratsit";
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
+  
+  const [apiWeather,setApiWeather] = useState({})
+
+  useEffect=()=>axios.get(url).then(res=> {
+    setApiWeather({name:res.data.name})
+  }, [])
+
+  useEffect=()=>(()=>{
+console.log(apiWeather);
+console.log('usEf');
+  },[apiWeather])
+
+
+  
+
   return (
     <div className={appS.mainWrapper}>
       <div className={appS.content}>
@@ -26,11 +47,11 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/setting" element={<Setting />} />
           <Route path="/Stack" element={<Stack />} />
-          <Route path="/test" element={<TestApi />} />
+          <Route path="/test" element={<TestApi apiWeather={apiWeather}/>} />
           <Route path="*" element={<Error />} />
         </Routes>
       </div>
-      <pre>               !     !     !     2     !     !     !      </pre>
+      <pre>               !     !     !     3     !     !     !      </pre>
 
       <footer className={appS.footer}>
         <NavBottom />
--- src/components/TestApi/TestApi.jsx
+++ src/components/TestApi/TestApi.jsx
@@ -1,50 +1,18 @@
-import axios, { Axios } from "axios";
-import { useEffect, useState } from "react";
-
-function TestApi() {
-  let apiKey = "928346fa14b100bf560110b1c5fef477";
-  let city = "Antratsit";
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`;
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(10);
-
-  const onCount = () => {
-    setCount(count + 1);
-  };
-  const onCount2 = () => {
-    setCount2(count2 + 1);
-  };
-  const logCount = () => {
-    console.log(count);
-  };
-  const logCount2 = () => {
-    console.log(count2);
-  };
-
-
-  useEffect(() => {
-    // Обновляем заголовок документа с помощью API браузера
-    console.log("useEffect");
-  }, [count]);
 
 
 
-fetch(url)
-.then(data=>data.text())
-.then(data2=>console.log(data2))
+function TestApi(props) {
+  // const [dataPoint, setDataPoint] = useState(false);
+  // const [dataApi, setDataApi] = useState({});
 
+  
+      console.log(props.name);
 
-
+  
   return (
-    <>
-      <div>City:{count}</div>
-      <div>Temp:</div>
-      <div>Humidity:</div>
-      <button onClick={onCount}>{count}: коунт</button>
-      <button onClick={onCount2}>{count2}:коунт 2</button>
-      <button onClick={logCount}>LOG</button>
-      <button onClick={logCount2}>LOG2</button>
-    </>
+<>
+<div>{props.name}</div>
+</>
   );
 }
 
